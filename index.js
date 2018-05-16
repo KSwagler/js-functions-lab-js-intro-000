bdd3952fe3474037b047893b0bace4a2c2c86738
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-16 14:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays (string) {
 return "Happy holidays!"
 }
 function happyHolidaysTo (name) {
-  return 'Happy holidays, you!'
+  return 'Happy holidays, ${name}!'
 }
 function happyHolidaysTo (holiday, name) {
   return 'Happy ${holidays}, ${you}!'
